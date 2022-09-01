4cca9e72bc2283720eec03e62c46a1f3525df560
hwlei888 <leihuawen888@gmail.com>
2022-09-01 13:19:18 +1000

    before search

--- app/assets/javascripts/collocations_form.js
+++ app/assets/javascripts/collocations_form.js
@@ -2,8 +2,7 @@
 
 $(function(){
     $("#collocation_image_link button").on("click", function(){
-        $("#collocation_image_link").append(`<div>
-        <br>
+        $("#collocation_image_link").append(`<div><br>
         <input type="text" name="photo_links[]" class="collocation_photo_link" />
 
         </div>`)
